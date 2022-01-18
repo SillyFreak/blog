2ca8c3ec7851753213fc9b572e92ac04dced46e0
Clemens Koza <clemens.koza@gmx.at>
2022-01-18 22:51:38 +0100

    refactor logic into allPosts

--- src/lib/posts/allPosts.ts
+++ src/lib/posts/allPosts.ts
@@ -1,10 +1,35 @@
-export default async function allPosts() {
+import type { SvelteComponent } from 'svelte';
+
+export type Metadata = {
+	title: string;
+	published: Date;
+	edited: Date | null;
+};
+
+export type PostMetadata = {
+	slug: string;
+	metadata: Metadata;
+};
+
+export type Post = PostMetadata & {
+	content: typeof SvelteComponent;
+};
+
+export default async function allPosts(): Promise<Post[]> {
 	const posts = Object.entries(import.meta.glob('./*.svelte.md'));
 	return Promise.all(
 		posts.map(async ([path, resolver]) => {
-			const slug = path.slice(2, -10);
 			const post = await resolver();
-			return { slug, post };
+
+			const slug = path.slice(2, -10).split('-', 2).pop();
+			const content = post.default;
+
+			let { published, edited, ...more } = post.metadata;
+			published = new Date(published);
+			edited = edited === null ? null : new Date(edited);
+			const metadata = { published, edited, ...more };
+
+			return { slug, metadata, content };
 		}),
 	);
 }
---
+++ src/routes/blog/[yyyy]/[mm]/[dd]/[slug].svelte
@@ -0,0 +1,43 @@
+<script context="module" lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
+	import allPosts from '$lib/posts/allPosts';
+
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ params }) {
+		const { yyyy, mm, dd, slug } = params;
+		const date = new Date(`${yyyy}-${mm}-${dd}`);
+
+		const posts = await allPosts();
+		const post = posts.filter(({ slug: slugCandidate, metadata }) => {
+			return slug === slugCandidate && +date - +metadata.published === 0;
+		});
+
+		if (post.length === 1) {
+			const { slug, metadata, content } = post[0];
+			return { props: { slug, ...metadata, content } };
+		}
+
+		return {
+			status: 404,
+			error: new Error(`no post at /${yyyy}/${mm}/${dd}/${slug}`),
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let slug: string;
+	export let title: string;
+	export let published: Date;
+	export let edited: Date;
+	export let content: typeof SvelteComponent;
+</script>
+
+<h1>{title}</h1>
+
+<p>
+	Published {published.toLocaleDateString()}
+	{#if edited !== null}- last edited: {edited.toLocaleDateString()}{/if}
+</p>
+
+<svelte:component this={content} />
