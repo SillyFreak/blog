51c4267085c99c669e744e2fdb1d4171077cae69
Clemens Koza <clemens.koza@gmx.at>
2022-01-19 23:53:18 +0100

    redirect home to blog

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script context="module" lang="ts">
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ fetch }) {
+		return { status: 301, redirect: '/blog/' };
+	}
+</script>
