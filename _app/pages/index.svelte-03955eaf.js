440d55c19b19a7d07aa877493ecc35eba09e6f12
SillyFreak <SillyFreak@users.noreply.github.com>
2022-01-19 22:53:49 +0000

    deploy: 51c4267085c99c669e744e2fdb1d4171077cae69

---
+++ _app/pages/index.svelte-03955eaf.js
@@ -0,0 +1,1 @@
+import{S as e,i as n,s as o}from"../chunks/vendor-2338cb50.js";async function r({fetch:s}){return{status:301,redirect:"/blog/"}}class u extends e{constructor(t){super();n(this,t,null,null,o,{})}}export{u as default,r as load};
