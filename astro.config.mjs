5fecf6f971d73f1c4cb0078e6b3d259f453d7f9e
Clemens Koza <clemens.koza@gmx.at>
2025-05-31 16:29:22 +0200

    integrate Typst

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,38 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { typst } from "astro-typst";
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://blog.sillyfreak.space',
+  base: '/',
+
+  // Whether to prefetch links while hovering.
+  // See: https://docs.astro.build/en/guides/prefetch/
+  prefetch: {
+    prefetchAll: true,
+  },
+
+  integrations: [
+    sitemap(),
+    typst({
+      // Always builds HTML files
+			target: "html",
+			// conditionally build HTML or SVG
+			// target: () => "html",
+    }),
+  ],
+
+  vite: {
+    build: {
+      assetsInlineLimit(filePath, content) {
+        const KB = 1024;
+        return content.length < (filePath.endsWith(".css") ? 100 * KB : 4 * KB);
+      },
+    },
+    ssr: {
+      external: ["@myriaddreamin/typst-ts-node-compiler"],
+    },
+  },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,17 +2,18 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Load Typst files in the `content/blog/` directory.
+  loader: glob({ base: './content/blog', pattern: '**/*.typ' }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    author: z.string().optional(),
+    description: z.any().optional(),
+    date: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
