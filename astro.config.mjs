ccf68b993447721430c8de8553637da5e74ff99c
Clemens Koza <clemens.koza@gmx.at>
2025-05-31 16:31:02 +0200

    format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { typst } from "astro-typst";
+import { typst } from 'astro-typst';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.sillyfreak.space',
+  site: 'https://blog.sillyfreak.space',
   base: '/',
 
   // Whether to prefetch links while hovering.
@@ -18,9 +18,9 @@ export default defineConfig({
     sitemap(),
     typst({
       // Always builds HTML files
-			target: "html",
-			// conditionally build HTML or SVG
-			// target: () => "html",
+      target: 'html',
+      // conditionally build HTML or SVG
+      // target: () => "html",
     }),
   ],
 
@@ -28,11 +28,11 @@ export default defineConfig({
     build: {
       assetsInlineLimit(filePath, content) {
         const KB = 1024;
-        return content.length < (filePath.endsWith(".css") ? 100 * KB : 4 * KB);
+        return content.length < (filePath.endsWith('.css') ? 100 * KB : 4 * KB);
       },
     },
     ssr: {
-      external: ["@myriaddreamin/typst-ts-node-compiler"],
+      external: ['@myriaddreamin/typst-ts-node-compiler'],
     },
   },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "SillyFreak's Blog";
-export const SITE_DESCRIPTION = "Ramblings about programming and other stuff";
+export const SITE_DESCRIPTION = 'Ramblings about programming and other stuff';
