a9b9d9b8095582f61154f52086480175d1a6790e
Clemens Koza <clemens.koza@gmx.at>
2026-01-17 21:19:30 +0100

    change main blog URL & title: sillyfreak -> ensko

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.sillyfreak.space',
+  site: 'https://blog.ensko.at',
   base: '/',
 
   // Whether to prefetch links while hovering.
--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "SillyFreak's Blog";
+export const SITE_TITLE = "ensko's blog";
 export const SITE_DESCRIPTION = 'Ramblings about programming and other stuff';
